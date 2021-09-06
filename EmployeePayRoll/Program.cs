d42d34ca7fde353831780f9aafc537b6864b7f09
saurabhs <saurabhpshirsath@gmail.com>
2021-09-06 21:26:25 +0530

    [saurabh]Add:Connection Establised To Database

---
+++ EmployeePayRoll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayRoll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll ADO");
+        }
+    }
+}
